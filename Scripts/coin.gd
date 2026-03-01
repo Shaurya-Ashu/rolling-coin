c517eec29c03c991fcc739a936f28a2e01fd5c82
shaurya ashu <shauryaashu023@gmail.com>
2026-03-01 07:50:54 +0530

    Initial commit

---
+++ Scripts/coin.gd
@@ -0,0 +1,59 @@
+extends CharacterBody2D
+
+
+var speed = 100
+var jump_force = -220
+var gravity = 900
+
+var is_dead = false
+
+@onready var anim = $AnimatedSprite2D
+
+
+# =====================================
+# MAIN PHYSICS LOOP
+# =====================================
+func _physics_process(delta):
+
+	if is_dead:
+		return
+
+	# -------- GRAVITY --------
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+
+	# -------- LEFT / RIGHT --------
+	var direction = Input.get_axis("move left", "move right")
+	velocity.x = direction * speed
+
+
+	# -------- JUMP --------
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_force
+
+
+	move_and_slide()
+
+	update_animation(direction)
+
+
+# =====================================
+# ANIMATION SYSTEM
+# =====================================
+func update_animation(direction):
+
+	if not is_on_floor():
+		anim.play("jump")
+
+	elif direction == 0:
+		anim.play("idle")
+
+	elif direction > 0:
+		anim.play("move_right")
+
+	else:
+		anim.play("move_left")
+
+
+# =====================================
